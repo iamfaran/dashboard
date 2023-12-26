b60d27d4ffb14be9b3726ec4046610253f37afc7
Faran Javed <faran1997@outlook.com>
2023-12-26 18:02:18 +0500

    make space for main content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
+import Main from "./components/layout/Main";
 
 function App() {
   const [rightSidebar, setRightSidebar] = useState(false);
@@ -14,40 +15,45 @@ function App() {
   return (
     <>
       <Header toggleRightSidebar={toggleRightSidebar} />
-      <Sidebar isOpen={rightSidebar} pos={"left-0"}>
-        <div className="flex flex-col justify-between h-full p-5">
-          <div>
-            <div className="text-gray-500">WELCOME BACK</div>
-            <div className="text-xl font-bold mb-4">Hannah</div>
+      <div className="flex">
+        <Sidebar isOpen={rightSidebar} pos={"left-0"}>
+          <div className="flex flex-col justify-between h-full p-5">
+            <div>
+              <div className="text-gray-500">WELCOME BACK</div>
+              <div className="text-xl font-bold mb-4">Hannah</div>
 
-            <div className="text-gray-500">BALANCE</div>
-            <div className="text-xl font-bold mb-4">$79.87</div>
+              <div className="text-gray-500">BALANCE</div>
+              <div className="text-xl font-bold mb-4">$79.87</div>
 
-            <div className="flex items-center justify-between">
-              <div className="text-gray-500">ADDRESS</div>
+              <div className="flex items-center justify-between">
+                <div className="text-gray-500">ADDRESS</div>
+              </div>
+              <div className="mb-4">123 ABC ST.</div>
+
+              <div className="text-blue-500 cursor-pointer">+ ADD ADDRESS</div>
             </div>
-            <div className="mb-4">123 ABC ST.</div>
 
-            <div className="text-blue-500 cursor-pointer">+ ADD ADDRESS</div>
-          </div>
-
-          <div>
-            <div className="uppercase font-bold text-gray-500 mb-2">
-              More options
+            <div>
+              <div className="uppercase font-bold text-gray-500 mb-2">
+                More options
+              </div>
+              <ul>
+                <li className="mb-2">Profile Settings</li>
+                <li className="mb-2">My Orders</li>
+                <li className="mb-2">Earnings</li>
+                <li className="mb-2">FAQ</li>
+                <li className="mb-2">Partner Program</li>
+                <li className="mb-2">Contact Us</li>
+                <li className="mb-2">Log Out</li>
+              </ul>
             </div>
-            <ul>
-              <li className="mb-2">Profile Settings</li>
-              <li className="mb-2">My Orders</li>
-              <li className="mb-2">Earnings</li>
-              <li className="mb-2">FAQ</li>
-              <li className="mb-2">Partner Program</li>
-              <li className="mb-2">Contact Us</li>
-              <li className="mb-2">Log Out</li>
-            </ul>
           </div>
+        </Sidebar>
+        <div className="p-5  mt-[var(--header-height)] md:ml-64 md:mr-64 ">
+          <Main />
         </div>
-      </Sidebar>
-      <Sidebar pos={"right-0"}></Sidebar>
+        <Sidebar pos={"right-0"}></Sidebar>
+      </div>
     </>
   );
 }
---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>HAHAHAHA OUCH YOO</div>;
+};
+
+export default Main;
